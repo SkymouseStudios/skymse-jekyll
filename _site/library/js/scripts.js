0c8849ef466fa275648ceadd14cc711761326606
Patrick Hanus <patrick@skymse.com>
2017-08-29 22:55:38 -0400

    Initial Commit
    I wasn’t aware that I hadn’t committed yet…

--- _site/library/js/scripts.js
+++ _site/library/js/scripts.js
@@ -4,45 +4,45 @@
  */
 
 // Help Scout Beacon
-	function(e, o, n) {
-	    window.HSCW = o, window.HS = n, n.beacon = n.beacon || {};
-	    var t = n.beacon;
-	    t.userConfig = {}, t.readyQueue = [], t.config = function(e) {
-	        this.userConfig = e
-	    }, t.ready = function(e) {
-	        this.readyQueue.push(e)
-	    }, o.config = {
-	        docs: {
-	            enabled: !1,
-	            baseUrl: ""
-	        },
-	        contact: {
-	            enabled: !0,
-	            formId: "f3be6a56-39b7-11e7-9841-0ab63ef01522"
-	        }
-	    };
-	    var r = e.getElementsByTagName("script")[0],
-	        c = e.createElement("script");
-	    c.type = "text/javascript", c.async = !0, c.src = "https://djtflbt20bdde.cloudfront.net/", r.parentNode.insertBefore(c, r)
-	}(document, window.HSCW || {}, window.HS || {});
+// 	function(e, o, n) {
+// 	    window.HSCW = o, window.HS = n, n.beacon = n.beacon || {};
+// 	    var t = n.beacon;
+// 	    t.userConfig = {}, t.readyQueue = [], t.config = function(e) {
+// 	        this.userConfig = e
+// 	    }, t.ready = function(e) {
+// 	        this.readyQueue.push(e)
+// 	    }, o.config = {
+// 	        docs: {
+// 	            enabled: !1,
+// 	            baseUrl: ""
+// 	        },
+// 	        contact: {
+// 	            enabled: !0,
+// 	            formId: "f3be6a56-39b7-11e7-9841-0ab63ef01522"
+// 	        }
+// 	    };
+// 	    var r = e.getElementsByTagName("script")[0],
+// 	        c = e.createElement("script");
+// 	    c.type = "text/javascript", c.async = !0, c.src = "https://djtflbt20bdde.cloudfront.net/", r.parentNode.insertBefore(c, r)
+// 	}(document, window.HSCW || {}, window.HS || {});
 
-// Config for the beacon
-HS.beacon.config({
-    color: '#cacaca',
-    translation: {
-      searchLabel: 'En que podemos ayudarte?',
-      searchErrorLabel: 'Lo sentimos, hubo un error con tu busqueda'
-    },
-    topics: [
-      { val: 'need-help', label: 'Need help with the product' },
-      { val: 'bug', label: 'I think I found a bug'}
-    ],
-    attachment: true,
-    instructions:'This is instructional text that goes above the form.',
-    contactSuccessLabel:'Bombs away!'
-	contactSuccessDescription:'Your message has been sent, check out your email for a confirmation email and ',
-	sendLabel: 'Boom!'
-  });
+// // Config for the beacon
+// HS.beacon.config({
+//     color: '#cacaca',
+//     translation: {
+//       searchLabel: 'En que podemos ayudarte?',
+//       searchErrorLabel: 'Lo sentimos, hubo un error con tu busqueda'
+//     },
+//     topics: [
+//       { val: 'need-help', label: 'Need help with the product' },
+//       { val: 'bug', label: 'I think I found a bug'}
+//     ],
+//     attachment: true,
+//     instructions:'This is instructional text that goes above the form.',
+//     contactSuccessLabel:'Bombs away!'
+// 	contactSuccessDescription:'Your message has been sent, check out your email for a confirmation email and ',
+// 	sendLabel: 'Boom!'
+//   });
 // End HelpScout Beacon
 
 function updateViewportDimensions() {
@@ -54,8 +54,8 @@ var viewport = updateViewportDimensions();
 
 // Mobile Menu Slider Function
 function mobileMenu() {
-  jQuery('div#menu-burger').on('click', function() {
-    jQuery('nav#menu').toggleClass('menu-shown');
+  jQuery('#menu-burger').on('click',function() {
+    jQuery('#mobile-menu').toggleClass('menu-shown');
 });
 }
 
